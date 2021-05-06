0ee5dbb4bd6a3626668da345e6d5c137b61ffbfa
pabtejada <65474537+pabtejada@users.noreply.github.com>
2021-05-06 11:31:15 -0500

    Services

---
+++ EZLabor.API/Services/FreelancerKnowledgeService.cs
@@ -0,0 +1,71 @@
+﻿using EZLabor.API.Domain.Models;
+using EZLabor.API.Domain.Persistence.Repositories;
+using EZLabor.API.Domain.Services;
+using EZLabor.API.Domain.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZLabor.API.Services
+{
+    public class FreelancerKnowledgeService : IFreelancerKnowledgeService
+    {
+        private readonly IFreelancerKnowledgeRepository _freelancerKnowledgeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FreelancerKnowledgeService(IFreelancerKnowledgeRepository freelancerKnowledgeRepository, IUnitOfWork unitOfWork)
+        {
+            _freelancerKnowledgeRepository = freelancerKnowledgeRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<FreelancerKnowledgeResponse> AssignFreelancerKnowledgeAsync(int freelancerId, int knowledgeId)
+        {
+            try
+            {
+
+                await _freelancerKnowledgeRepository.AssignFreelancerKnowledge(freelancerId, knowledgeId);
+                await _unitOfWork.CompleteAsync();
+
+                FreelancerKnowledge freelancerKnowledge = await _freelancerKnowledgeRepository.FindByFreelancerIdAndKnowledgeId(freelancerId, knowledgeId);
+
+                return new FreelancerKnowledgeResponse(freelancerKnowledge);
+            }
+            catch (Exception ex)
+            {
+                return new FreelancerKnowledgeResponse($"An error ocurred while assigning Knowledge to Freelancer: {ex.Message}");
+            }
+        }
+
+        public async Task<IEnumerable<FreelancerKnowledge>> ListAsync()
+        {
+            return await _freelancerKnowledgeRepository.ListAsync();
+        }
+
+        public async Task<IEnumerable<FreelancerKnowledge>> ListByFreelancerIdAsync(int freelancerId)
+        {
+            return await _freelancerKnowledgeRepository.ListByFreelancerIdAsync(freelancerId);
+        }
+
+        public async Task<IEnumerable<FreelancerKnowledge>> ListByKnowledgeIdAsync(int knowledgeId)
+        {
+            return await _freelancerKnowledgeRepository.ListByKnowledgeIdAsync(knowledgeId);
+        }
+
+        public async Task<FreelancerKnowledgeResponse> UnassignFreelancerKnowledgeAsync(int freelancerId, int knowledgeId)
+        {
+            try
+            {
+                FreelancerKnowledge freelancerKnowledge = await _freelancerKnowledgeRepository.FindByFreelancerIdAndKnowledgeId(freelancerId, knowledgeId);
+                _freelancerKnowledgeRepository.UnassignFreelancerKnowledge(freelancerId, knowledgeId);
+                await _unitOfWork.CompleteAsync();
+
+                return new FreelancerKnowledgeResponse(freelancerKnowledge);
+            }
+            catch (Exception ex)
+            {
+                return new FreelancerKnowledgeResponse($"An error ocurred while unassigning Knoledge to Freelancer: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ EZLabor.API/Services/FreelancerService.cs
@@ -0,0 +1,112 @@
+﻿using EZLabor.API.Domain.Models;
+using EZLabor.API.Domain.Persistence.Repositories;
+using EZLabor.API.Domain.Services;
+using EZLabor.API.Domain.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZLabor.API.Services
+{
+    public class FreelancerService: IFreelancerService
+    {
+        private readonly IFreelancerRepository _freelancerRepository;
+        private readonly IFreelancerKnowledgeRepository _freelancerKnowledgeRepository;
+        private readonly IProposalRepository _proposalRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public FreelancerService(IFreelancerRepository freelancerRepository, IFreelancerKnowledgeRepository freelancerKnowledgeRepository, IProposalRepository proposalRepository, IUnitOfWork unitOfWork)
+        {
+            _freelancerRepository = freelancerRepository;
+            _freelancerKnowledgeRepository = freelancerKnowledgeRepository;
+            _proposalRepository = proposalRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<FreelancerResponse> DeleteAsync(int id)
+        {
+            var existingFreelancer = await _freelancerRepository.FindById(id);
+
+            if (existingFreelancer == null)
+                return new FreelancerResponse("Freelancer not found");
+
+            try
+            {
+                _freelancerRepository.Remove(existingFreelancer);
+                await _unitOfWork.CompleteAsync();
+
+                return new FreelancerResponse(existingFreelancer);
+            }
+            catch (Exception ex)
+            {
+                return new FreelancerResponse($"An error ocurred while deleting the freelancer: {ex.Message}");
+            }
+        }
+
+        public async Task<FreelancerResponse> GetByIdAsync(int id)
+        {
+            var existingFreelancer = await _freelancerRepository.FindById(id);
+
+            if (existingFreelancer == null)
+                return new FreelancerResponse("Freelancer not found");
+            return new FreelancerResponse(existingFreelancer);
+        }
+
+        public async Task<IEnumerable<Freelancer>> ListAsync()
+        {
+            return await _freelancerRepository.ListAsync();
+        }
+
+        public async Task<IEnumerable<Freelancer>> ListByEmployerId(int employerId)
+        {
+            var proposals = await _proposalRepository.ListByEmployerIdAsync(employerId);
+            var freelancers = proposals.Select(pt => pt.Freelancer).ToList();
+            return freelancers;
+        }
+
+        public async Task<IEnumerable<Freelancer>> ListByKnowledgeId(int knowledgeId)
+        {
+            var freelancerKnowledges = await _freelancerKnowledgeRepository.ListByKnowledgeIdAsync(knowledgeId);
+            var freelancers = freelancerKnowledges.Select(pt => pt.Freelancer).ToList();
+            return freelancers;
+        }
+
+        public async Task<FreelancerResponse> SaveAsync(Freelancer freelancer)
+        {
+            try
+            {
+                await _freelancerRepository.AddAsync(freelancer);
+                await _unitOfWork.CompleteAsync();
+
+                return new FreelancerResponse(freelancer);
+            }
+            catch (Exception ex)
+            {
+                return new FreelancerResponse($"An error ocurred while saving the freelancer: {ex.Message}");
+            }
+        }
+
+        public async Task<FreelancerResponse> UpdateAsync(int id, Freelancer freelancer)
+        {
+            var existingFreelancer = await _freelancerRepository.FindById(id);
+
+            if (existingFreelancer == null)
+                return new FreelancerResponse("Freelancer not found");
+
+            existingFreelancer.UserName = freelancer.UserName;
+
+            try
+            {
+                _freelancerRepository.Update(existingFreelancer);
+                await _unitOfWork.CompleteAsync();
+
+                return new FreelancerResponse(existingFreelancer);
+            }
+            catch (Exception ex)
+            {
+                return new FreelancerResponse($"An error ocurred while updating the freelancer: {ex.Message}");
+            }
+        }
+    }
+}
