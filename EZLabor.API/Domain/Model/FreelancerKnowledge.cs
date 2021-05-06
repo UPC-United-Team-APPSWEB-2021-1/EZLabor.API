6a34e7cabb06b8a3152cf12a916bccfacc013d84
alr28 <83131314+alr28@users.noreply.github.com>
2021-05-06 11:27:40 -0500

    Domain added

---
+++ EZLabor.API/Domain/Model/FreelancerKnowledge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZLabor.API.Domain.Models
+{
+    public class FreelancerKnowledge
+    {
+        public int FreelancerId { get; set; }
+        public Freelancer Freelancer { get; set; }
+        public int KnowledgeId { get; set; }
+        public Knowledge Knowledge { get; set; }
+    }
+}
---
+++ EZLabor.API/Domain/Model/Knowledge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZLabor.API.Domain.Models
+{
+    public class Knowledge
+    {
+        public int Id { get; set; }
+        public string TechnologyName { get; set; }
+        public string CertificateUrl { get; set; }
+        public List<FreelancerKnowledge> FreelancerKnowledges { get; set; }
+    }
+}
