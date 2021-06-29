e26b3190f37635fe540a258d5165eb3a05bfca18
pabtejada <pabtejada@gmail.com>
2021-06-29 18:08:09 -0500

    feature/tokens

---
+++ EZLabor.API/Accounts/Domain/Services/Communications/AuthenticationResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZLabor.API.Accounts.Domain.Services.Communications
+{
+    public class AuthenticationResponse
+    {
+    }
+}
